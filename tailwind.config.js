17307a61fe3fa6d251d34de80cf518b30bf40fc5
Likeur off <102558960+Likeur@users.noreply.github.com>
2023-11-06 05:11:23 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  Darkmode : 'class',
+  content: [
+    "./**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
